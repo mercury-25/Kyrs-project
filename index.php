26394228165c84212a9abdbc2af3ba2975f4e0dd
Дмитрий Глазвин <sony183@bk.ru>
2016-11-18 11:06:39 +0300

    upd

---
+++ index.php
@@ -0,0 +1,94 @@
+﻿<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Авторизация на сайте:</title>
+<link rel="stylesheet" type="text/css" href="css/style.css" />
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
+<style>
+body {
+   background-color: #284166;
+  background-image: url("https://www.transparenttextures.com/patterns/45-degree-fabric-light.png");
+/* This is mostly intended for prototyping; please download the pattern and re-host for production environments. Thank you! */
+}	
+	.btn { display: inline-block; *display: inline; *zoom: 1; padding: 4px 10px 4px; margin-bottom: 0; font-size: 13px; line-height: 18px; color: #333333; text-align: center;text-shadow: 0 1px 1px rgba(255, 255, 255, 0.75); vertical-align: middle; background-color: #f5f5f5; background-image: -moz-linear-gradient(top, #ffffff, #e6e6e6); background-image: -ms-linear-gradient(top, #ffffff, #e6e6e6); background-image: -webkit-gradient(linear, 0 0, 0 100%, from(#ffffff), to(#e6e6e6)); background-image: -webkit-linear-gradient(top, #ffffff, #e6e6e6); background-image: -o-linear-gradient(top, #ffffff, #e6e6e6); background-image: linear-gradient(top, #ffffff, #e6e6e6); background-repeat: repeat-x; filter: progid:dximagetransform.microsoft.gradient(startColorstr=#ffffff, endColorstr=#e6e6e6, GradientType=0); border-color: #e6e6e6 #e6e6e6 #e6e6e6; border-color: rgba(0, 0, 0, 0.1) rgba(0, 0, 0, 0.1) rgba(0, 0, 0, 0.25); border: 1px solid #e6e6e6; -webkit-border-radius: 4px; -moz-border-radius: 4px; border-radius: 4px; -webkit-box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.05); -moz-box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.05); box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.05); cursor: pointer; *margin-left: .3em; }
+.btn:hover, .btn:active, .btn.active, .btn.disabled, .btn[disabled] { background-color: #e6e6e6; }
+.btn-large { padding: 9px 14px; font-size: 15px; line-height: normal; -webkit-border-radius: 5px; -moz-border-radius: 5px; border-radius: 5px; }
+.btn:hover { color: #333333; text-decoration: none; background-color: #e6e6e6; background-position: 0 -15px; -webkit-transition: background-position 0.1s linear; -moz-transition: background-position 0.1s linear; -ms-transition: background-position 0.1s linear; -o-transition: background-position 0.1s linear; transition: background-position 0.1s linear; }
+.btn-primary, .btn-primary:hover { text-shadow: 0 -1px 0 rgba(0, 0, 0, 0.25); color: #ffffff; }
+.btn-primary.active { color: rgba(255, 255, 255, 0.75); }
+.btn-primary { background-color: #4a77d4; background-image: -moz-linear-gradient(top, #6eb6de, #4a77d4); background-image: -ms-linear-gradient(top, #6eb6de, #4a77d4); background-image: -webkit-gradient(linear, 0 0, 0 100%, from(#6eb6de), to(#4a77d4)); background-image: -webkit-linear-gradient(top, #6eb6de, #4a77d4); background-image: -o-linear-gradient(top, #6eb6de, #4a77d4); background-image: linear-gradient(top, #6eb6de, #4a77d4); background-repeat: repeat-x; filter: progid:dximagetransform.microsoft.gradient(startColorstr=#6eb6de, endColorstr=#4a77d4, GradientType=0);  border: 1px solid #3762bc; text-shadow: 1px 1px 1px rgba(0,0,0,0.4); box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.5); }
+.btn-primary:hover, .btn-primary:active, .btn-primary.active, .btn-primary.disabled, .btn-primary[disabled] { filter: none; background-color: #4a77d4; }
+.btn-block { width: 100%; display:block; }
+
+</style>
+</head>
+<body>
+<div align="center"><h2>Введите данные для входа в учетную запись</h2>
+ <section class="login-form-wrap">
+<form class="login-form" action="index.php" method="post">
+    <label>
+      <input type="email" name="login" required placeholder="Ваше имя">
+    </label>
+    <label>
+      <input type="password" name="password" required placeholder="Ваш пароль">
+    </label>
+    <input type="submit" name="submit">
+  </form>
+</section>
+</div>
+<?php $connection = mysqli_connect('localhost', 'root', '', 'base') or die(mysqli_error()); // Соединение с базой данных ?>
+
+<?php if (isset($_POST['submit'])) // Отлавливаем нажатие кнопки "Отправить"
+{
+if (empty($_POST['login'])) // Если поле логин пустое
+{
+echo '<script>alert("Поле логин не заполненно");</script>'; // То выводим сообщение об ошибке
+}
+elseif (empty($_POST['password'])) // Если поле пароль пустое
+{
+echo '<script>alert("Поле пароль не заполненно");</script>'; // То выводим сообщение об ошибке
+}
+else  // Иначе если все поля заполненны
+{    
+$login = $_POST['login']; // Записываем логин в переменную 
+$password = $_POST['password']; // Записываем пароль в переменную           
+$query = mysqli_query($connection, "SELECT `id` FROM `users` WHERE `login` = '$login' AND `password` = '$password'"); // Формируем переменную с запросом к базе данных с проверкой пользователя
+$result = mysqli_fetch_array($query); // Формируем переменную с исполнением запроса к БД 
+if (empty($result['id'])) // Если запрос к бд не возвразяет id пользователя
+{
+echo '<script>alert("Неверные Логин или Пароль");</script>'; // Значит такой пользователь не существует или не верен пароль
+}
+else // Если возвращяем id пользователя, выполняем вход под ним
+{
+$_SESSION['password'] = $password; // Заносим в сессию  пароль
+require_once 'redirect.html';
+$_SESSION['login'] = $login; // Заносим в сессию  логин
+$_SESSION['id'] = $result['id']; // Заносим в сессию  id
+echo '<div align="center">Вы успешно вошли в систему: '.$_SESSION['login'].'</div>'; // Выводим сообщение что пользователь авторизирован        
+}     
+}		
+} ?>
+<br>
+
+<?php if (isset($_GET['exit'])) { // если вызвали переменную "exit"
+unset($_SESSION['password']); // Чистим сессию пароля
+unset($_SESSION['login']); // Чистим сессию логина
+unset($_SESSION['id']); // Чистим сессию id
+} ?>
+
+<?php if (isset($_SESSION['login']) && isset($_SESSION['id'])) // если в сессии загружены логин и id
+{
+echo '<div align="center"><a href="index.php?exit" class="main-action">Выход</a></div>'; // Выводим нашу ссылку выхода
+} ?>
+
+<?php if (!isset($_SESSION['login']) || !isset($_SESSION['id'])) // если в сессии не загружены логин и id
+{
+echo '<div id="lastmain">
+			<a href="reg.php" class="main-action">Создать &nbsp;<i class="fa fa-smile-o" aria-hidden="true"></i> &mdash; учетную запись</a>
+		</div>'; // Выводим нашу ссылку регистрации
+} ?>
+
+</div>
+</body>
+</html>
---
+++ reg.php
@@ -0,0 +1,72 @@
+﻿<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Регистрация на сайте</title>
+<link rel="stylesheet" type="text/css" href="css/style.css" />
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
+ <style>
+body {
+   background-color: #284166;
+  background-image: url("https://www.transparenttextures.com/patterns/45-degree-fabric-light.png");
+/* This is mostly intended for prototyping; please download the pattern and re-host for production environments. Thank you! */
+}	
+</style>   
+
+</head>
+<body>
+
+
+<div align="center"><h2>Регистрация на сайте:</h2>
+ <section class="login-form-wrap">
+<form class="login-form" action="reg.php" method="post">
+    <label>
+      <input type="email" name="login2" required placeholder="Ваше имя">
+    </label>
+    <label>
+      <input type="password" name="password2" required placeholder="Ваш пароль">
+    </label>
+    <input type="submit" name="submit2">
+  </form>
+</section>
+</div>
+
+
+<?php if (!isset($_SESSION['login']) || !isset($_SESSION['id'])) // если в сессии не загружены логин и id
+{
+echo '<div id="lastmain">
+<a href="index.php" class="main-action">Вернуться назад </a>
+</div>'; // Выводим нашу ссылку регистрации
+} 
+?>
+
+
+
+
+
+
+<?php $connection = mysqli_connect('localhost', 'root', '', 'base') or die(mysqli_error()); // Соединение с базой данных ?> 
+
+<?php if (isset($_POST['submit2'])) // Отлавливаем нажатие на кнопку отправить 
+{
+if (empty($_POST['login2']))  // Условие - если поле логин пустое
+{
+echo "<script>alert('Поле логин не заполненно');</script>"; // Выводим сообщение об ошибке
+}          
+elseif (empty($_POST['password2'])) // Иначе если поле с паролем пустое
+{
+echo "<script>alert('Поле логин не заполненно');</script>"; // Выводим сообщение об ошибке
+}                      
+else // Иначе если поля не пустые
+{
+$login2 = $_POST['login2']; // Присваеваем переменной значение из поля с логином             
+$password2 = $_POST['password2']; // Присваеваем другой переменной значение из поля с паролем
+$query = "INSERT INTO `users` (login, password) VALUES ('$login2', '$password2')"; // Создаем переменную с запросом к базе данных на отправку нового юзера
+$result = mysqli_query($connection, $query) or die(mysql_error()); // Отправляем переменную с запросом в базу данных 
+echo "<div align='center'>Регистрация прошла успешно!</div>"; // Сообщаем что все получилось
+}
+} 
+?>
+
+</body>
+</html>
